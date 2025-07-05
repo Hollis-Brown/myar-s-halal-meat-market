b7c13a597e8abf8c905ab6df06f5e31ec8b2ab87
Hollis-Brown <hollis.brown.cohort231@gmail.com>
2025-07-04 18:44:12 -0700

    Remove swiper import and related code

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
-import 'swiper/css';
-import 'swiper/css/pagination';
+
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
